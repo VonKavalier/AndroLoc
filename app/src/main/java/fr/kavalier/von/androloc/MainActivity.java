8c00465a579d32354bda5b6e3ce78bdb24477b76
VonKavalier <celestin.rocker.tom15@gmail.com>
2017-08-26 13:20:41 +0200

    Mise en place du bouton quitter

--- app/src/main/java/fr/kavalier/von/androloc/MainActivity.java
+++ app/src/main/java/fr/kavalier/von/androloc/MainActivity.java
@@ -2,6 +2,8 @@ package fr.kavalier.von.androloc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bouton_quitter = (Button)findViewById(R.id.bouton_quitter);
+        bouton_quitter.setOnClickListener(bQuitterListener);
     }
+
+    private View.OnClickListener bQuitterListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            finish();
+        }
+    };
 }
